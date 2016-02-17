f873f1b553e051f45002102b2c33c33a75cd0a1a
Athi <mateusz.piekarczyk@me.com>
2016-02-17 18:23:31 +0100

    Initial commit.

---
+++ src/test/java/com/athifx/AthiFXApplicationTest.java
@@ -0,0 +1,14 @@
+package com.athifx;
+
+import org.junit.Before;
+
+/**
+ * Created by Athi on 17.02.2016.
+ */
+public class AthiFXApplicationTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
