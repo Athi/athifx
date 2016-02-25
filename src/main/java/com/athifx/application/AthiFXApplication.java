720eb3abc34cdd42407405374ef9ed14af8521cf
Athi <mateusz.piekarczyk@me.com>
2016-02-25 17:41:04 +0100

    Removed unnecessary code in the AthiFXApplication.

--- src/main/java/com/athifx/application/AthiFXApplication.java
+++ src/main/java/com/athifx/application/AthiFXApplication.java
@@ -6,8 +6,6 @@ import com.athifx.application.menu.group.Groups;
 import com.athifx.application.menu.item.Items;
 import com.google.inject.Inject;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import javax.annotation.PostConstruct;
@@ -30,17 +28,9 @@ public abstract class AthiFXApplication<GROUPS extends Enum<?> & Groups, ITEMS e
     public void start(Stage primaryStage) throws Exception {
         AthiFXInjector.createInjector(this);
 
-        primaryStage.setTitle("Hello World");
-        VBox root = new VBox();
-        primaryStage.setScene(new Scene(root, 300, 275));
 
-
-        System.out.println("VIEWS:" + applicationConfiguration.getViews());
-        System.out.println("MENU_ITEMS:" + applicationConfiguration.getMenuItems());
-
-        System.out.println("GROUPS" + getGroups());
-
-        System.out.println("ITEMS" + getItems());
+        List<GROUPS> groups = getGroups();
+        List<ITEMS> items = getItems();
 
         primaryStage.show();
     }
