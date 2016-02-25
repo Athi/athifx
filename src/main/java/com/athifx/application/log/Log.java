a24e4e32ca705bbf9b5be7a0e3286094efe8afbb
Athi <mateusz.piekarczyk@me.com>
2016-02-25 21:57:03 +0100

    Changes to the Log.java - creation of handler before each logging and same with closing (it wont create multiple log files for each logger initialization), Added tests for Log.java and logfile deletion after test extending AthiFXApplicationTestCase.java.

--- src/main/java/com/athifx/application/log/Log.java
+++ src/main/java/com/athifx/application/log/Log.java
@@ -8,27 +8,22 @@ import java.util.logging.*;
  */
 public class Log {
 
-    private final static String LOGGER_NAME = "APPLICATION_LOGGER";
-    private final static Logger logger = Logger.getLogger(LOGGER_NAME);
+    public final static String DEFAULT_LOGGER_NAME = "APPLICATION_LOGGER";
+    public static final String DEFAULT_LOG_FILE_NAME = "application.log";
 
-    private static final String DEFAULT_LOG_NAME = "application.log";
+    private final static Logger LOGGER = Logger.getLogger(DEFAULT_LOGGER_NAME);
+    private Handler handler;
 
     private final Class<?> logClass;
+    private final String fileName;
 
     private Log(Class<?> logClass, String fileName) {
         this.logClass = logClass;
-        try {
-            Handler handler = new FileHandler(fileName, true);
-            handler.setFormatter(new SimpleFormatter());
-            logger.addHandler(handler);
-            logger.setLevel(Level.ALL);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.fileName = fileName;
     }
 
     public static Log getLogger(Class<?> logClass) {
-        return new Log(logClass, DEFAULT_LOG_NAME);
+        return new Log(logClass, DEFAULT_LOG_FILE_NAME);
     }
 
     public static <FILE extends Enum & LogFile> Log getLogger(Class<?> logClass, FILE logFile) {
@@ -36,15 +31,36 @@ public class Log {
     }
 
     public void error(String message, Throwable throwable) {
-        logger.logp(AthiLoggingLevel.ERROR, logClass.getName(), "", message, throwable);
+        initHandler();
+        LOGGER.logp(AthiLoggingLevel.ERROR, logClass.getName(), "", message, throwable);
+        closeHandler();
     }
 
     public void warning(String message, Throwable throwable) {
-        logger.logp(AthiLoggingLevel.WARNING, logClass.getName(), "", message, throwable);
+        initHandler();
+        LOGGER.logp(AthiLoggingLevel.WARNING, logClass.getName(), "", message, throwable);
+        closeHandler();
     }
 
     public void info(String message) {
-        logger.logp(AthiLoggingLevel.INFO, logClass.getName(), "", message);
+        initHandler();
+        LOGGER.logp(AthiLoggingLevel.INFO, logClass.getName(), "", message);
+        closeHandler();
+    }
+
+    private void initHandler() {
+        try {
+            handler = new FileHandler(fileName, true);
+            handler.setFormatter(new SimpleFormatter());
+            LOGGER.addHandler(handler);
+            LOGGER.setLevel(Level.ALL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeHandler() {
+        handler.close();
     }
 
     public interface LogFile {
