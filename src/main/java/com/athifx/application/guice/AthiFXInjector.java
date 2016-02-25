4d6bd79b87e2ee3ea83ea31bb264c8aa4d2b1abf
Athi <mateusz.piekarczyk@me.com>
2016-02-25 17:36:34 +0100

    refs #3 - added support for classes annotated with SessionScoped (It will make always  inject the same instance of the annotated object), added generic recognition of the AthiFXInjector (parameter need's to extend the AthiFXApplication class)

--- src/main/java/com/athifx/application/guice/AthiFXInjector.java
+++ src/main/java/com/athifx/application/guice/AthiFXInjector.java
@@ -4,21 +4,43 @@ import com.athifx.application.AthiFXApplication;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.matcher.Matchers;
+import org.reflections.Reflections;
+
+import javax.enterprise.context.SessionScoped;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Athi
  */
 public class AthiFXInjector {
 
-    public static void createInjector(AthiFXApplication self) {
+    public static <T extends AthiFXApplication> void createInjector(T self) {
         Guice.createInjector(new AbstractModule() {
             @Override
             protected void configure() {
                 bind(AthiFXApplication.class).toInstance(self);
+
+                getClassAndInstanceToBind().entrySet().forEach(classObjectEntry -> bind(classObjectEntry.getKey()).toInstance(classObjectEntry.getValue()));
 
                 bindListener(Matchers.any(), new PostConstructInjectionListener());
             }
         });
     }
 
+    private static Map<Class<Object>, Object> getClassAndInstanceToBind() {
+        Map<Class<Object>, Object> classObjectToBind = new HashMap<>();
+
+        Reflections reflections = new Reflections();
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(SessionScoped.class);
+        for (Class<?> type : typesAnnotatedWith) {
+            try {
+                classObjectToBind.put((Class<Object>) type, type.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return classObjectToBind;
+    }
 }
