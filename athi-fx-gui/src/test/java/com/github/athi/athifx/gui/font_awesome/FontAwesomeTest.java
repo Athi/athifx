4dea503dcde11deca217d51fbbb2bb5d20b7f063
Athi <mateusz.piekarczyk@me.com>
2016-06-14 22:13:01 +0200

    Poprawka w FontAwesome.java oraz dopisanie testu FontAwesomeTest.java

---
+++ athi-fx-gui/src/test/java/com/github/athi/athifx/gui/font_awesome/FontAwesomeTest.java
@@ -0,0 +1,36 @@
+package com.github.athi.athifx.gui.font_awesome;
+
+import javafx.scene.control.Label;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Athi
+ */
+public class FontAwesomeTest {
+
+    @Test
+    public void unicodeTest() {
+        String expectedYoutubeSquareUnicode = "\uF166";
+        assertEquals(expectedYoutubeSquareUnicode, FontAwesome.YOUTUBE_SQUARE.getUnicode());
+    }
+
+    @Test
+    public void fontSizeStyleTest() {
+        String expected = "-fx-font-size: 16px;";
+        assertEquals(expected, FontAwesome.FONT_SIZE_STYLE(16));
+    }
+
+    //TODO
+//    @Test
+//    public void labelIconTest() {
+//        Label label = FontAwesome.labelIcon(FontAwesome.ADJUST, 16);
+//        assertEquals(label.getText(), FontAwesome.ADJUST.getUnicode());
+//        assertTrue(label.getStyleClass().contains(FontAwesome.STYLE_CLASS_ICONS));
+//        assertTrue(label.getStyle().contains(FontAwesome.FONT_SIZE_STYLE(16)));
+//    }
+
+}
