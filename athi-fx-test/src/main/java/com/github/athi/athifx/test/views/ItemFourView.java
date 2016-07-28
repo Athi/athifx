be1cf4a1db95fca12b35a08a1fba7855ee9ee2bd
Athi <mateusz.piekarczyk@gmail.com>
2016-07-28 08:58:36 +0200

    Changed View implementations (simplification). Removed bad code. In future it will be implemented to show all possible functions.

--- athi-fx-test/src/main/java/com/github/athi/athifx/test/views/ItemFourView.java
+++ athi-fx-test/src/main/java/com/github/athi/athifx/test/views/ItemFourView.java
@@ -1,13 +1,11 @@
 package com.github.athi.athifx.test.views;
 
 import com.github.athi.athifx.gui.menu.item.DisableEnableAMenuItemEvent;
-import com.github.athi.athifx.gui.navigation.navigator.Navigator;
 import com.github.athi.athifx.gui.navigation.view.AView;
 import com.github.athi.athifx.gui.navigation.view.View;
 import com.github.athi.athifx.test.configuration.TestItems;
 import com.google.common.eventbus.EventBus;
 import com.google.inject.Inject;
-import com.google.inject.name.Named;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -19,41 +17,24 @@ import javax.annotation.PostConstruct;
 @View(itemId = 4)
 public class ItemFourView extends AView {
 
-    @Inject
-    private Navigator navigator;
+    private static final String VIEW_NAME = ItemFourView.class.getSimpleName();
 
     @Inject
     private EventBus eventBus;
 
-    @Inject
-    @Named("test")
-    private String test;
-
-    @Inject
-    @Named("ini:test_header:test")
-    private String testFromIni;
-
     @PostConstruct
     public void initItemFourView() {
+        getChildren().add(new Label(VIEW_NAME));
 
-        getChildren().add(new Label("FROM PROPERTIES: " + test));
-        getChildren().add(new Label("FROM INI: " + testFromIni));
-
-        getChildren().add(new Label("ITEM FOUR VIEW"));
-        Button test = new Button("Navigate to ItemOne");
-        test.setOnAction(event -> navigator.navigateTo(TestItems.ITEM_ONE));
-
-        Button disableButton = new Button("Disable item3");
+        Button disableButton = new Button("Disable/Enable item3");
         disableButton.setOnAction(event -> eventBus.post(new DisableEnableAMenuItemEvent(TestItems.ITEM_FIVE)));
 
-        this.getChildren().addAll(test, disableButton);
+        this.getChildren().addAll(disableButton);
     }
 
     @Override
     public void enter() {
-        System.out.println("TEST ITEM FOUR ENTER");
-        System.out.println(test);
-        System.out.println(testFromIni);
+        System.out.println("ENTER: " + VIEW_NAME);
     }
 
 }
--- athi-fx-test/src/main/java/com/github/athi/athifx/test/views/ItemOneView.java
+++ athi-fx-test/src/main/java/com/github/athi/athifx/test/views/ItemOneView.java
@@ -1,16 +1,10 @@
 package com.github.athi.athifx.test.views;
 
-import com.github.athi.athifx.gui.font_awesome.FontAwesome;
 import com.github.athi.athifx.gui.navigation.navigator.Navigator;
 import com.github.athi.athifx.gui.navigation.view.AView;
 import com.github.athi.athifx.gui.navigation.view.View;
-import com.github.athi.athifx.test.configuration.TestItems;
-import com.github.athi.athifx.test.views.window.ItemOneTestWindow;
 import com.google.inject.Inject;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
 
 /**
  * Created by Athi
@@ -18,46 +12,18 @@ import javafx.scene.layout.FlowPane;
 @View(itemId = 1)
 public class ItemOneView extends AView {
 
+    private static final String VIEW_NAME = ItemOneView.class.getSimpleName();
+
     @Inject
     private Navigator navigator;
 
     public ItemOneView() {
-        getChildren().add(new Label("ITEM ONE VIEW"));
-        getChildren().add(createIconLabel("\uf115", 16));
-        getChildren().add(new TextField());
-        Button test1 = new Button("Navigate1 to ItemFour");
-        test1.setOnAction(event -> navigator.navigateTo(TestItems.ITEM_FOUR));
-        Button test2 = new Button("Navigate2 to ItemFour");
-        test2.setOnAction(event -> navigator.navigateTo(TestItems.ITEM_FOUR));
-        Button test3 = new Button("Navigate3 to ItemFour");
-        test3.setOnAction(event -> navigator.navigateTo(TestItems.ITEM_FOUR));
-
-        Button test4 = createIconButton(FontAwesome.PLUS.getUnicode(), "TEST3", 15);
-
-
-        Button test5 = createIconButton(FontAwesome.TELEVISION.getUnicode(), "TEST_WINDOW", 15);
-        test5.setOnAction(event -> new ItemOneTestWindow(200, 200).show());
-        this.getChildren().add(new FlowPane(test1, test2, test3, test4, test5)); // IMPORTANT FlowPane is horizontal and wraps content
-    }
-
-    public static Label createIconLabel(String iconName, int iconSize) {
-        Label label = new Label(iconName);
-        label.getStyleClass().add("icons");
-        label.setStyle("-fx-font-size: " + iconSize + "px;");
-        return label;
-    }
-
-    public static Button createIconButton(String iconName, String text, int iconSize) {
-        Label icon = createIconLabel(iconName, iconSize);
-        Button button = new Button();
-        button.setText(text);
-        button.setGraphic(icon);
-        return button;
+        getChildren().add(new Label(VIEW_NAME));
     }
 
     @Override
     public void enter() {
-        System.out.println("TEST ITEM ONE ENTER");
+        System.out.println("ENTER: " + VIEW_NAME);
     }
 
 }
